76e1d49b8726f5b3d0768e6446f7d8b8d6f23703
Mohammed Ghaith <maf.ghaith@yahoo.com>
2025-04-24 21:05:06 +0300

    Initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pandas as pd
+
+# st.html("""
+# <style>
+# body, html {
+#     direction: RTL;
+#     unicode-bidi: bidi-override;
+#     text-align: right;
+# }
+# p, div, input, label, h1, h2, h3, h4, h5, h6 {
+#     direction: RTL;
+#     unicode-bidi: bidi-override;
+#     text-align: right;
+# }
+# </style>
+# """)
+
+stock_df = pd.read_csv('./input.csv', encoding='utf8')
+stock_df.drop(columns=['Unnamed: 23', 'Unnamed: 24'], inplace=True, errors='ignore')
+
+markets = stock_df['السوق'].unique()
+
+
+if __name__ == "__main__":
+    st.header('Stock Analyzer v0.01')
+    st.divider()
+    options = st.multiselect(
+    "Choose the market",
+    markets,
+    markets,)
+    dividens = st.toggle("Dividens only")
+    st.divider()
+    filtered_df = stock_df[stock_df['السوق'].isin(options)]
+    if dividens:
+        filtered_df = filtered_df[~filtered_df['توزيعات الأرباح'].isin(['0.00%', 'Unavailable', None])]
+    st.dataframe(filtered_df,
+                hide_index=True,
+                column_config={
+                    "السعر": None
+
+                })
+
+    # st.html("""
+    # <style>
+    # [data-testid=stElementToolbarButton]:first-of-type {
+    #     display: none;
+    # }
+    # </style>
+    # """)
+    # with st.container(height=300):
+    #     st.html(stock_df.to_html())
